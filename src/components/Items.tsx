ade7878bb4444fdfd8b87a3fed31f56d2166d57f
alishba-ansari35 <alishbaansari180@gmail.com>
2024-12-09 18:47:52 +0500

    done

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -25,65 +25,6 @@ const Items = () => {
 </div>
 
 
-
-      {/* <div className=" grid grid-cols-1 gap-8 lg:grid-cols-[auto_1fr] px-5"> */}
-
-        {/* Images section */}
-        {/* <div className="flex flex-col md:flex-row items-center gap-8"> */}
-          {/* Orange chair (left side) */}
-          {/* <div className="w-full md:w-[48%]">
-            <Image
-              src="/images/card3.png"
-              alt="card3"
-              className="h-full w-full object-cover"
-              width={500}
-              height={500}
-              priority
-            />
-          </div> */}
-
-          {/* Other chairs (right side) */}
-          {/* <div className="grid grid-cols-2 gap-4 w-full md:w-[60%] lg:w-[48%]">
-            <div className="aspect-square">
-              <Image
-                src="/images/card4.png"
-                alt="card4"
-                className="h-full w-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="aspect-square">
-              <Image
-                src="/images/card2.png"
-                alt="card2"
-                className="h-full w-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="aspect-square">
-              <Image
-                src="/images/card8.png"
-                alt="card8"
-                className="h-full w-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="aspect-square">
-              <Image
-                src="/images/card1.png"
-                alt="card1"
-                className="h-full w-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
-
     
     </div>
     </main>
